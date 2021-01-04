87867898033add2dcb17eed02d59de04b0911f1a
Eric Stratton <strattonericj@gmail.com>
2021-01-04 11:07:43 -0800

    add Edit functionality to Courses
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- Registrar/Controllers/CoursesController.cs
+++ Registrar/Controllers/CoursesController.cs
@@ -43,5 +43,19 @@ namespace Registrar.Controllers
           .FirstOrDefault(course => course.CourseId == id);
       return View(thisCourse);
     }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Entry(course).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
