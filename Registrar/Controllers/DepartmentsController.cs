2320f975a54390850a8b9e69f7ce3298595c4206
Eric Stratton <strattonericj@gmail.com>
2021-01-05 11:51:04 -0800

    add corresponding Department controllers
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- Registrar/Controllers/DepartmentsController.cs
+++ Registrar/Controllers/DepartmentsController.cs
@@ -21,5 +21,42 @@ namespace Registrar.Controllers
       List<Department> model = _db.Departments.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Department department)
+    {
+      _db.Departments.Add(department);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisDepartment = _db.Departments
+          .Include(Department => Department.Courses)
+          .ThenInclude(join => join.Course)
+          .FirstOrDefault(Department => Department.DepartmentId == id);
+      return View(thisDepartment);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisDepartment = _db.Departments.FirstOrDefault(department => department.DepartmentId == id);
+      return View(thisDepartment);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Department department)
+    {
+      _db.Entry(department).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
