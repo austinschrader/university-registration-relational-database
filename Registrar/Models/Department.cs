0b9d5101aaeafe5c535540d79b908ee29c1533e7
Eric Stratton <strattonericj@gmail.com>
2021-01-05 13:54:45 -0800

    change Hash Set to DepartmentCourse
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- Registrar/Models/Department.cs
+++ Registrar/Models/Department.cs
@@ -6,12 +6,12 @@ namespace Registrar.Models
   {
     public Department()
     {
-      this.Courses = new HashSet<CourseStudent>();
+      this.Courses = new HashSet<DepartmentCourse>();
     }
 
     public int DepartmentId { get; set; }
     public string Name { get; set; }
 
-    public virtual ICollection<CourseStudent> Courses { get; set; }
+    public virtual ICollection<DepartmentCourse> Courses { get; set; }
   }
 }
